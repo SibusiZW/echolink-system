1b381ec03d9dd92b24189a10c9466588760dd5a3
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-02-16 16:02:41 +0200

    feat: added brief intro to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>j</div>
+    <div className="space-y-8">
+      <h1 className="text-3xl text-gray-700 font-bold">Welcome to EchoLink!</h1>
+      <p>This is the official, school-wide and emergency an announcements system of St Patrick's High</p>
+    </div>
   );
 }
