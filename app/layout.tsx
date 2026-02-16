505729cf2d7fc9f994f0a3d85c21bbea6da07b06
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-02-16 15:30:19 +0200

    feat: added main Inter font to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { inter } from "@/lib/fonts";
 import "./globals.css";
 
 
@@ -15,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={``}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
