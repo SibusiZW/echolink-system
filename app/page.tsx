241503d648369b107f9c0b6ae0eca4fcb5e71163
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-02-17 10:29:54 +0200

    feat: finished home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import StatCard from "@/components/stats-card";
 import { getAllAnnouncementsCount, getImportantAnnouncementsCount } from "@/server/announcements";
-import { getAllEmergenciesCount } from "@/server/emergencies";
-import { Megaphone, OctagonAlert, Shield } from "lucide-react";
+import { getAllEmergenciesCount, getCriticalEmergenciesCount } from "@/server/emergencies";
+import { BadgeAlert, Megaphone, OctagonAlert, Shield } from "lucide-react";
 
 export default async function HomePage() {
   const announcementsCount = await getAllAnnouncementsCount()
   const importantAnnouncementsCount = await getImportantAnnouncementsCount()
   const emergenciesCount = await getAllEmergenciesCount()
+  const criticalEmergenciesCount = await getCriticalEmergenciesCount()
 
   return (
     <div className="space-y-8">
@@ -18,6 +19,7 @@ export default async function HomePage() {
         <StatCard title="All Announcements" value={announcementsCount} icon={<Megaphone className="text-blue-500" size={24}/>}/>
         <StatCard title="Important Announcements" value={importantAnnouncementsCount} icon={<Shield className="text-orange-500" size={24}/>}/>
         <StatCard title="All Emergencies" value={emergenciesCount} icon={<OctagonAlert className="text-red-500" size={24}/>}/>
+        <StatCard title="Critical Emergencies" value={criticalEmergenciesCount} icon={<BadgeAlert className="text-red-800" size={24}/>}/>
       </div>
 
     </div>
