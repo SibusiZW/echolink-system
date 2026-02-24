122e75213b510191a7223df64b6d9bc8fed88947
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-02-24 15:48:07 +0200

    feat: created critical emergencies page

--- app/emergencies/critical/page.tsx
+++ app/emergencies/critical/page.tsx
@@ -1,14 +1,14 @@
 import { getEmergencies } from "@/server/emergencies"
-import { OctagonAlert } from "lucide-react"
+import { BadgeAlert } from "lucide-react"
 
-export default async function EmergenciesPage() {
+export default async function CriticalEmergenciesPage() {
     const emergencies = await getEmergencies();
 
     return (
         <div className="space-y-8">
-            <h1 className="text-4xl text-gray-600">Emergencies</h1>
-            {emergencies.map(item => <div key={item.id} className="space-y-4 p-5 border border-red-500 rounded-md">
-                <OctagonAlert className="text-red-500" size={30}/>
+            <h1 className="text-4xl text-gray-600">Critical Emergencies</h1>
+            {emergencies.map(item => <div key={item.id} className="space-y-4 p-5 border border-red-800 rounded-md">
+                <BadgeAlert className="text-red-800" size={30}/>
                 <h1 className="text-gray-500 text-xl">{item.title}</h1>
                 <p>
                     {item.description}
