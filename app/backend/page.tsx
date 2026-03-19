5f0658084cec7a203f924a01126b97289f3bef7b
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-19 18:21:01 +0200

    added announcements page for backend

--- app/backend/page.tsx
+++ app/backend/page.tsx
@@ -1,9 +1,25 @@
-import AdminSidebar from "@/components/admin-sidebar"
+import { getAnnouncements } from "@/server/announcements"
+import { Shield } from "lucide-react";
 
-export default function BackendPage() {
+export default async function BackendPage() {
+    
+    const announcements = await getAnnouncements();
+
     return (
-        <div>
-            u
+        <div className="space-y-8">
+            <h1 className="text-4xl text-gray-600">Announcements</h1>
+            {announcements.map(item => <div key={item.id} className="bg-white space-y-4 p-5 border border-blue-500 rounded-md">
+                <Shield className="text-blue-500" size={30}/>
+                <h1 className="text-gray-500 text-xl">{item.title}</h1>
+                <p>
+                    {item.description}
+                </p>
+
+                <p>
+                    Priority: {item.priority}
+                </p>
+                <span className="text-gray-500">Posted on: {new Date(item.created_at).toLocaleDateString()}</span>
+            </div>)}
         </div>
     )
 }
