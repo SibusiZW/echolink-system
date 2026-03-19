f8ae37aa1a380fef1520448620cc0f8e14c770f0
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-19 19:22:29 +0200

    added update functionality

--- components/dialogs/edit-ann-button.tsx
+++ components/dialogs/edit-ann-button.tsx
@@ -1,12 +1,32 @@
 'use client';
 
-import { NotebookPen, Save } from "lucide-react";
+import { Loader2, NotebookPen, Save } from "lucide-react";
 import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
+import React, { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
 
 export default function EditAnnButton({ id }: { id: any }) {
+
+    const [record, setRecord] = useState<any>();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const getRecord = async () => {
+            const { data } = await supabase.from('announcements').select('*').eq('id', id).single();
+            setRecord(data);
+        }
+        getRecord();
+    }, [])
+
+    async function handleUpdate(e: React.SubmitEvent) {
+        setLoading(true);
+        await supabase.from('announcements').update({ title: record.title, description: record.description }).eq('id', record.id)
+        setLoading(false);
+    }
+
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -18,13 +38,12 @@ export default function EditAnnButton({ id }: { id: any }) {
             <DialogContent className="text-center">
                 <DialogTitle>Confirm editing</DialogTitle>
 
-                <form>
-                    <Input placeholder="Edit title" className="mb-2"/>
-                    <Textarea placeholder="Edit description" className="mb-2"/>
+                <form onSubmit={handleUpdate}>
+                    <Input value={record?.title} placeholder="Edit title" className="mb-2" onChange={(e) => setRecord({...record, title: e.target.value })}/>
+                    <Textarea value={record?.description} placeholder="Edit description" className="mb-2" onChange={(e) => setRecord({ ...record, description: e.target.value })}/>
 
-                    <Button className="w-full">
-                        <Save />
-                        Save record
+                    <Button type="submit" className="w-full">
+                        {loading ? <Loader2 className="animate-spin"/>: <><Save /> Save record</>}
                     </Button>
                 </form>
                 <DialogDescription>
