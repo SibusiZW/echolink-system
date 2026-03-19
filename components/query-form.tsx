241f7ec9403c4dc1e9ad7e9372e2ee47ac03ffc5
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-03-19 18:13:00 +0200

    added loading animation to query button

--- components/query-form.tsx
+++ components/query-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Send } from "lucide-react";
+import { Loader2, Send } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
@@ -10,13 +10,30 @@ import { toast } from "sonner";
 
 export default function QueryForm() {
 
+    function renderContent() {
+        if (!loading) {
+            return (
+            <>
+                <Send />
+                Send query
+            </>
+        )
+        } else {
+            return <Loader2 className="animate-spin" />
+        }
+    }
+
     const [message, setMessage] = useState("");
     const [sender, setSender] = useState("");
     const [err, setErr] = useState("");
+    const [loading, setLoading] = useState(false);
 
     async function handleSubmit(e: React.SubmitEvent) {
         e.preventDefault();
+
+        setLoading(true);
         const { error } = await supabase.from('queries').insert({ message: message, sender: sender })
+        setLoading(false);
 
         if (error) {
             setErr(error?.message)
@@ -37,8 +54,7 @@ export default function QueryForm() {
                 <Textarea value={message} className="mb-2" placeholder="Enter a query to improve the system" onChange={(e) => setMessage(e.target.value)} required/>
                 <Input value={sender} className="mb-4" placeholder="Enter your name" onChange={(e) => setSender(e.target.value)} required/>
                 <Button type="submit" className="bg-green-500 hover:bg-green-200 w-full">
-                    <Send />
-                    Send query
+                    {renderContent()}
                 </Button>
             </form>
         </div>
